ada0f0ff2fc86316c0cc0ac97b45542fce425afc
Bit-Blocks <bit-blocks@outlook.com>
2024-05-12 20:58:04 -0500

    Some styles and colors are updated

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -15,19 +15,21 @@ export default {
       gray: colors.gray,
       indigo: colors.indigo,
       neutral: colors.neutral,  // Used mainly for text color
-      yellow: {
-        50: "#fefce8",
-        100: "#fef9c3",
-        400: "#facc15",
-        500: "#eab308",
+      yellow: {   //Indigo
+        '50': '#eef2ff',
+        '100': '#e0e7ff',
+        '200': '#c7d2fe',
+        '300': '#a5b4fc',
+        '400': '#818cf8',
+        '500': '#6366f1',
       }, // Accent colors, used mainly for star color, heading and buttons
-      orange: {
-        100: "#ffedd5",
-        200: "#fed7aa",
-        300: "#fb713b",
-        400: "#fa5a15",
-        500: "#e14d0b",
-        600: "#ea580c",
+      orange: {   //Indigo
+        '100': '#c7d2fe',
+        '200': '#a5b4fc',
+        '300': '#818cf8',
+        '400': '#6366f1',
+        '500': '#4f46e5',
+        '600': '#4338ca',
       }, // Primary colors, used mainly for links, buttons and svg icons
       red: colors.red, // Used for bookmark icon
       zinc: colors.zinc, // Used mainly for box-shadow
